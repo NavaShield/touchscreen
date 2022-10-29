ac39a6af89e977e841f6c3d68dba04719cab2d3a
overlord of cum <88568085+ChenzIsHomo@users.noreply.github.com>
2022-10-29 16:25:52 +0100

    Create rocket-preload-links-js-extra.js

---
+++ rocket-preload-links-js-extra.js
@@ -0,0 +1,9 @@
+      var RocketPreloadLinksConfig = {
+        "excludeUris": "\/touch-results\/|\/(.+\/)?feed\/?.+\/?|\/(?:.+\/)?embed\/|\/(index\\.php\/)?wp\\-json(\/.*|$)|\/wp-admin\/|\/logout\/|\/wp-login.php",
+        "usesTrailingSlash": "1",
+        "imageExt": "jpg|jpeg|gif|png|tiff|bmp|webp|avif",
+        "fileExt": "jpg|jpeg|gif|png|tiff|bmp|webp|avif|php|pdf|html|htm",
+        "siteUrl": "https:\/\/tecagile.com",
+        "onHoverDelay": "100",
+        "rateThrottle": "3"
+      };
